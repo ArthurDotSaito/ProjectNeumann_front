4301435f33b2932240ee61fcddd1592f38c2dc3f
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-04 19:37:04 -0300

    chore: adding toastify and submit event

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,6 @@
 import Header from '@/components/Header/Header';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 import { HomepageContainer } from '@/styles/indexStyle';
 import { useState } from 'react';
 
@@ -6,6 +8,23 @@ export default function Register() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
+	const router = useRouter();
+
+	async function submit(event: React.FormEvent) {
+		event.preventDefault();
+
+		if (password !== confirmPassword) {
+			toast('As senhas devem ser iguais!');
+		} else {
+			try {
+				await signUp(email, password);
+				toast('Inscrito com sucesso! Por favor, faça login.');
+				router.push('/login');
+			} catch (error) {
+				toast('Não foi possível fazer o cadastro!');
+			}
+		}
+	}
 
 	return (
 		<div className="flex max-w-6xl mx-auto flex-col py-2 min-h-screen bg-[#17181C] text-white z-negative-2">
