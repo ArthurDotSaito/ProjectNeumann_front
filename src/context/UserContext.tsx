3bf36e9033f2bd9075a122ad132d62bebafa2e8b
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-04 21:41:45 -0300

    feat: adding userContext

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,12 +1,25 @@
-import { createContext } from 'react';
+import { createContext, ReactNode } from 'react';
 
 import useLocalStorage from '../hooks/useLocalStorage';
 
-const UserContext = createContext();
+type UserData = {
+	token: string;
+};
+
+type UserContextType = {
+	userData: UserData | null;
+	setUserData: (data: UserData | null) => void;
+};
+
+const UserContext = createContext<UserContextType | null>(null);
 export default UserContext;
 
-export function UserProvider({ children }) {
-	const [userData, setUserData] = useLocalStorage('userData', {});
+type UserProviderProps = {
+	children: ReactNode;
+};
+
+export function UserProvider({ children }: UserProviderProps) {
+	const [userData, setUserData] = useLocalStorage('userData', null);
 
 	return <UserContext.Provider value={{ userData, setUserData }}>{children}</UserContext.Provider>;
 }
