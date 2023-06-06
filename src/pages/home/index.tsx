4b436a954f6e6513f7ff4e83e5eb2dc5ebc28572
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-06 01:00:46 -0300

    fix: fixing style on Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/Header/Header';
 
 export default function Home() {
 	return (
-		<div className="flex max-w-6xl mx-auto flex-col py-2 min-h-screen bg-[#17181C] text-white z-negative-2">
+		<div className="flex max-w-6xl flex-col py-2 min-h-screen bg-[#17181C] text-white z-negative-2">
 			<Header></Header>
 		</div>
 	);
