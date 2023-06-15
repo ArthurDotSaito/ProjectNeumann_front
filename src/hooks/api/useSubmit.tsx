cc24af894585340ef8a1a0145aa5039e51ea2ac6
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-15 08:52:42 -0300

    feat: adding a apiHook to submitCode

---
+++ src/hooks/api/useSubmit.tsx
@@ -0,0 +1,16 @@
+import { submitCode } from '@/services/submissionApi';
+import useAsync from '../useAsync';
+import useToken from '../useToken';
+
+export default function createNewBooking() {
+	const token = useToken();
+
+	const {
+		data: newSubmitCode,
+		loading: submitLoading,
+		error: submitError,
+		act: submitNewCode,
+	} = useAsync((codeData) => submitCode(token, codeData), false);
+
+	return { newSubmitCode, submitLoading, submitError, submitNewCode };
+}
