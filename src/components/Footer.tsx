9f1d77b21f8ff83a701d50f350a20d22ccee516d
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-20 21:49:02 -0300

    feat: adding a footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { w } from 'windstitch';
+
+export default function Footer() {
+	return <FooterContainer>Footer do Project Neumann</FooterContainer>;
+}
+
+const FooterContainer = w.main(`
+    fixed bottom-0 w-full h-10 bg-gray-400 flex items-center justify-center
+`);
