fa255703084881b8d6e34e78bed5b11e8116f73a
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-24 19:26:43 -0300

    feat: adding comment api call

---
+++ src/services/postCommentApi.tsx
@@ -0,0 +1,6 @@
+import api from './api';
+
+export async function postComment(token: string, postId: string, content: string) {
+	const { data } = await api.post(`/comments/${postId}`, content, { headers: { Authorization: `Bearer ${token}` } });
+	return data;
+}
