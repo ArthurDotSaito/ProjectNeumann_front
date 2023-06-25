39e60a424f2ea75844e5220c4b51f30418330632
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-24 21:38:07 -0300

    feat: implementing getAllComments hook

---
+++ src/hooks/api/useGetComments.tsx
@@ -0,0 +1,16 @@
+import { getAllComments } from '@/services/getAllCommentsApi';
+import useAsync from '../useAsync';
+import useToken from '../useToken';
+
+export default function usePostComment() {
+	const token = useToken();
+
+	const {
+		data: getCommentData,
+		loading: getCommentLoading,
+		error: getCommentError,
+		act: getCommentFunction,
+	} = useAsync((postId) => getAllComments(token, postId), false);
+
+	return { getCommentData, getCommentLoading, getCommentError, getCommentFunction };
+}
